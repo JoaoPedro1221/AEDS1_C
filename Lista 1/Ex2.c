d08e97e92cbbad3ff5016ed5be2f3685d31497cf
João Pedro F. Marques <joaopedrofm@uol.com.br>
2020-09-06 03:21:18 -0300

    Create Ex2.c

---
+++ Lista 1/Ex2.c
@@ -0,0 +1,16 @@
+//2. Crie um registro (struct) ALUNO que contenha os seguintes campos:
+//int m a t ri c ul a ;
+//char nome [ 5 0 ] ;
+//int di a \_nasc ;
+//int mes\_nasc ;
+//int ano \_nasc ;
+//f l o a t nota ;
+//Escreva um programa que leia o número n de alunos a serem cadastrados. Em seguida, um vetor V com n posições deve ser alocado dinamicamente usando uma função que retorna um ponteiro do tipo ALUNO. O programa deve preencher o vetor V e imprimir o registro dos alunos aprovados, isto é, com nota maior ou igual a 6,0. Para executar tais tarefas devem ser criadas funções para:
+//(a) Alocar memória dinamicamente para um vetor do tipo ALUNO.
+//(b) Preencher o vetor de alunos.
+//(c) Imprimir o vetor de alunos.
+//(d) Preencher o registro de um aluno.
+//(e) Imprimir o registro de um aluno.
+
+#include <stdio.h>
+#include <stdlib.h>
