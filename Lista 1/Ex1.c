528417abcfa9ede791c32c1984083745a9fbe4a3
João Pedro F. Marques <joaopedrofm@uol.com.br>
2020-09-06 03:16:51 -0300

    Update Ex1.c

--- Lista 1/Ex1.c
+++ Lista 1/Ex1.c
@@ -1,3 +1,9 @@
+//Escreva um programa que leia um número l de linhas e um número c de colunas. Em seguida, um vetor V de tamanho l ∗ c e uma matriz M com l linhas e c colunas devem ser alocados dinamicamente. Seu programa deve executar as tarefas abaixo sequencialmente, através de chamadas de funções:
+//(a) Preencher o vetor V com valores inteiros digitados pelo usuário.
+//(b) Imprimir o vetor V na tela.
+//(c) Preencher a matriz M, sequencialmente, com os valores do vetor V .
+//(d) Imprimir a matriz M na tela.
+
 #include <stdio.h>
 #include <stdlib.h>
 
